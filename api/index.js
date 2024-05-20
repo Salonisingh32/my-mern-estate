545221ca46b22b9ecc6f586b49d0edb78a2fd3fb
salonisingh32 <salonisingh6955@gmail.com>
2024-05-20 16:13:41 +0530

    edit index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import { error } from 'console';
 dotenv.config();
 mongoose
 .connect(process.env.MONGO)
@@ -29,14 +30,14 @@ app.listen(3000, () => {
 app.use('/api/user',userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-app.use((req, res, next) => {
-    if (req.path.endsWith('.mjs')) {
-      res.type('application/javascript');
-    } else if (req.path.endsWith('.jsx')) {
-      res.type('text/javascript');
-    }
-    next();
-  });
+// app.use((req, res, next) => {
+//     if (req.path.endsWith('.mjs')) {
+//       res.type('application/javascript');
+//     } else if (req.path.endsWith('.jsx')) {
+//       res.type('text/javascript');
+//     }
+//     next();
+//   });
 
 app.use(express.static(path.join(_dirname, '/client/dist')));
 
@@ -46,6 +47,7 @@ app.get('*', (req, res) => {
 })
 
 app.use((err, req, res, next) => {
+    console.log(err)
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
