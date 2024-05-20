5467f5121d388009a6e6085bdd37c0ce8687d03c
salonisingh32 <salonisingh6955@gmail.com>
2024-05-20 16:18:29 +0530

    edit index.js

--- api/index.js
+++ api/index.js
@@ -30,14 +30,14 @@ app.listen(3000, () => {
 app.use('/api/user',userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-// app.use((req, res, next) => {
-//     if (req.path.endsWith('.mjs')) {
-//       res.type('application/javascript');
-//     } else if (req.path.endsWith('.jsx')) {
-//       res.type('text/javascript');
-//     }
-//     next();
-//   });
+app.use((req, res, next) => {
+    if (req.path.endsWith('.mjs')) {
+      res.type('application/javascript');
+    } else if (req.path.endsWith('.jsx')) {
+      res.type('text/javascript');
+    }
+    next();
+  });
 
 app.use(express.static(path.join(_dirname, '/client/dist')));
 
@@ -47,7 +47,7 @@ app.get('*', (req, res) => {
 })
 
 app.use((err, req, res, next) => {
-    console.log(err)
+    console.error(err)
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
