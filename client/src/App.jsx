f38c8a829e33cc24d47af98afc61d46e64ebb378
salonisingh32 <salonisingh6955@gmail.com>
2024-05-20 15:17:34 +0530

    Fix App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Signup from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from './components/privateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
